1061ff69294a3ce013ed102fbba7aa3f4e7a4346
routh-vishal <routhvishal36@gmail.com>
2025-08-19 00:26:29 +0530

    connected to supabase db

--- server/config/db.js
+++ server/config/db.js
@@ -2,19 +2,15 @@ import pkg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const { Client } = pkg;
 
 const db = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // Required for Supabase
 });
 
 db.connect()
-  .then(() => console.log('Connected to the database'))
+  .then(() => console.log('Connected to Supabase'))
   .catch((err) => console.error('Database connection error:', err));
 
 export default db;
